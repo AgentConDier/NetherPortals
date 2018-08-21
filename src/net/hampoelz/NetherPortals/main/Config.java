b6c7dd1653469e0feea1b292458b61b6e4c22323
AgentConDier <Darnok22221@gmx.de>
2018-08-22 00:51:34 +0200

    Update Config.java
    Added config value for wrong number of arguments message
    Increased config version because more messages were added
    Added config values for reload failure/success messages
    Fixed Grammar in line 36: You <do> not have permission...
    Added after teleport message
    Added world does not exist message
    Increased config version again

--- src/net/hampoelz/NetherPortals/main/Config.java
+++ src/net/hampoelz/NetherPortals/main/Config.java
@@ -10,26 +10,22 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-public class Config
-{
+public class Config {
 	public static File ConfigFile = new File("plugins/NetherPortals", "config.yml");
 	public static FileConfiguration Config = YamlConfiguration.loadConfiguration(ConfigFile);
 	
 	//----------------------------------------------------------------------------------------------------------------------\\
 	
-	public static void save() throws IOException
-	{
+	public static void save() throws IOException {
 		Config.save(ConfigFile);
 	}
 	
-	public static void load() throws FileNotFoundException, IOException, InvalidConfigurationException
-	{
+	public static void load() throws FileNotFoundException, IOException, InvalidConfigurationException {
 		Config.load(ConfigFile);
 	}
 	
-	public static void setConfig() throws IOException
-	{
-		int ConfigVersion = 1;
+	public static void setConfig() throws IOException {
+		int ConfigVersion = 3;
 		
 		int Version = Config.getInt("config.ConfigVersion");
 		
@@ -37,8 +33,17 @@ public class Config
 		{		
 			Config.set("config.ConfigVersion", ConfigVersion);
 			
-			Config.set("config.General.Messages.No Permissions", "&8[&6NetherPortals&8] &cYou not have permission to perform this command.");
+			Config.set("config.General.Messages.No Permissions", "&8[&6NetherPortals&8] &cYou do not have permission to perform this command.");
 			Config.set("config.General.Messages.Usage", "&8[&6NetherPortals&8] &8Usage: &7/NetherPortals [reload]");
+			Config.set("config.General.Messages.WrongArguments", "&8[&6NetherPortals&8] &cError: Expected one argument but received %n%");
+			Config.set("config.General.Messages.ReloadSuccess", "&8[&6NetherPortals&8] &6The config was successfully reloaded");
+			Config.set("config.General.Messages.ReloadFail", "&8[&6NetherPortals&8] &cAn error occurred while reloading the configuration");
+			Config.set("config.General.Messages.WorldNotExist", "&8[&6NetherPortals&8] &cThe specified world does not exist!");
+			Config.set("config.General.Messages.UseAfterTeleport", true);
+			Config.set("config.General.Messages.AfterTeleport", "&8[&6NetherPortals&8] &6You are now in %w%");
+			
+
+			Config.set("config.TravelAgent.UseTravelAgent", true);
 			
 			Config.set("config.DefaultWorld", "Hub");
 			
@@ -50,9 +55,7 @@ public class Config
 			Config.set("worlds.Survival_End.portalgoesto.END", "Survival");
 			
 			save();
-		}
-		else if (Version < ConfigVersion || Version > ConfigVersion)
-		{	
+		} else if (Version < ConfigVersion || Version > ConfigVersion) {	
 			File ConfigFileBackup = new File("plugins/NetherPortals", "old config [v" + Version + "].yml");
 			
 			FileUtils.copyFile(ConfigFile, ConfigFileBackup);
@@ -73,6 +76,41 @@ public class Config
 	public static String getUsage()
 	{
 		return Config.getString("config.General.Messages.Usage");
+	}
+	
+	public static String getArguments()
+	{
+		return Config.getString("config.General.Messages.WrongArguments");
+	}
+	
+	public static String getSuccess()
+	{
+		return Config.getString("config.General.Messages.ReloadSuccess");
+	}
+	
+	public static String getFail()
+	{
+		return Config.getString("config.General.Messages.ReloadFail");
+	}
+	
+	public static String getWorldError()
+	{
+		return Config.getString("config.General.Messages.WorldNotExist");
+	}
+	
+	public static Boolean doAfterTeleport()
+	{
+		return Config.getBoolean("config.General.Messages.UseAfterTeleport");
+	}
+	
+	public static String getTeleport()
+	{
+		return Config.getString("config.General.Messages.AfterTeleport");
+	}
+	
+	public static Boolean useTravelAgent()
+	{
+		return Config.getBoolean("config.TravelAgent.UseTravelAgent");
 	}
 	
 	public static String getDefaultWorld()
